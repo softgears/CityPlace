8c2c009f0a81a84d7b08c0eb620846912b1f4c89
Yuri Korshev <ykorshev@softgears.ru>
2014-03-20 17:35:35 +1100

    Несколько заглушек в мобильном приложении)

--- CityPlace.Mobile/views/home.js
+++ CityPlace.Mobile/views/home.js
@@ -1,7 +1,26 @@
 ﻿CityPlace_Mobile.home = function (params) {
 
+    var dataItems = [
+        {
+            key: 'Мероприятия',
+            items: [
+                {id: 1, title: "Событие 1"},
+                {id: 2, title: "Событие 2"},
+                {id: 3, title: "Событие 3"},
+            ]
+        },
+        {
+            key: 'Новости',
+            items: [
+                { id: 1, title: "Новость 1" },
+                { id: 2, title: "Новость 2" },
+                { id: 3, title: "Новость 3" },
+            ]
+        }
+    ];
+
     var viewModel = {
-//  Put the binding properties here
+        homeDataItems: dataItems
     };
 
     return viewModel;
